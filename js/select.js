0c2c3198b00b6e5d9a463c2d880e3ef48d8c67a9
diff-vc-vcl <sadekova.tasnima@huawei.com>
2022-03-24 21:39:57 +0300

    Add hybrid systems

--- js/select.js
+++ js/select.js
@@ -36,21 +36,6 @@ $('.dropdown-menu.conversion li').click(function () {
 			</tr> \
 			<tr> \
 				<td></td>\
-				<td><a href="https://github.com/Wendison/VQMIVC">VQMIVC</a></td> \
-                <td><a href="https://github.com/liusongxiang/ppg-vc">BNE-PPG-VC</a></td> \
-                <td>Ours</td> \
-			</tr> \
-			<tr> \
-				<td style="font-size: medium; text-align: right;"><i style="font-weight: 700;">ZSL</i>:</td> \
-				<td><audio controls=""><source src="resources/audio/conversion/' + folder + '/vqmivc_zsl.wav"></audio></td> \
-				<td><audio controls=""><source src="resources/audio/conversion/' + folder + '/ppgvc1_zsl.wav"></audio></td> \
-				<td><audio controls=""><source src="resources/audio/conversion/' + folder + '/diffvc_zsl.wav"></audio></td> \
-			</tr> \
-			<tr> \
-				<td><br></td> \
-			</tr> \
-			<tr> \
-				<td></td>\
 				<td>FS-PPG-VC</td> \
                 <td><a href="https://github.com/liusongxiang/ppg-vc">BNE-PPG-VC</a></td> \
                 <td>Ours</td> \
@@ -178,17 +163,6 @@ $('.dropdown-menu.cloning li').click(function () {
             <td>Tacotron-SMA</td> \
             <td>Grad-TTS</td> \
             <td>Ours</td> \
-        </tr> \
-        <tr> \
-            <td style="font-size: medium; text-align: right;"><i style="font-weight: 700;">ZSL</i>:</td> \
-            <td><audio controls=""><source src="resources/audio/cloning/' + pair.folder + '/stylespeech_zsl.wav"></audio></td> \
-            <td><audio controls=""><source src="resources/audio/cloning/' + pair.folder + '/fs_zsl.wav"></audio></td> \
-            <td><audio controls=""><source src="resources/audio/cloning/' + pair.folder + '/sma_zsl.wav"></audio></td> \
-            <td><audio controls=""><source src="resources/audio/cloning/' + pair.folder + '/gradtts_zsl.wav"></audio></td> \
-            <td><audio controls=""><source src="resources/audio/cloning/' + pair.folder + '/diffvc_zsl.wav"></audio></td> \
-        </tr> \
-        <tr> \
-            <td><br></td> \
         </tr> \
         <tr> \
             <td style="font-size: medium; text-align: right;"><i style="font-weight: 700;">FSL</i>:</td> \
